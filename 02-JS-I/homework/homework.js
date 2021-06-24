eeeb31835d76ca54c91f2d270e292d396f14fefa
program1 <briancabrer4@gmail.com>
2021-06-24 19:25:40 -0300

    deber completo completito

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -243,8 +243,7 @@ function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
-  //Escribe tu código aquí
-  function esVocal(letra) {
+  //Escribe tu código aquí{
     if (letra.length > 1) {
         return "Dato incorrecto";
     }
