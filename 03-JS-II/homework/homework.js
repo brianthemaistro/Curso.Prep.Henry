d23675961879ad89d3e9dbd14d0a88e9d3046a0a
program1 <briancabrer4@gmail.com>
2021-06-30 16:58:53 -0300

    deber completo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -171,7 +171,7 @@ function operadoresLogicos(num1, num2, num3) {
   if (num1 > num2 && num1 > num3); {
     return "Número 1 es mayor y positivo";
   }
- 
+    
 }
 
 function esPrimo(numero) {
@@ -215,11 +215,12 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (Number.isInteger(numero) && numero >= 100 ) {
+  if (Number.isInteger(numero) && (numero < 1000 && numero > 99) {
     return true
   }
   return false
 }
+
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
